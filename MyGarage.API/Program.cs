ca908822be0931e22ac73be7ecd191128684412e
Thibault GAUTHIER <tgauthier@expertime.com>
2024-02-29 17:11:36 +0100

    feat: add CORS support

--- MyGarage.API/Program.cs
+++ MyGarage.API/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
 builder.Services.AddScoped<IVehicleService, VehicleService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 // Map API endpoints
 app.MapVehicles();
